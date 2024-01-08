3037de2de3184f2d51bcbefe3fa97aaab8cbc860
Tom Mo <tmo217@protonmail.com>
2024-01-08 22:44:14 +0800

    intro 80% with animations

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,13 +1,21 @@
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
+import {motion} from "framer-motion"; 
 
 type Props = {}
 
 export default function Intro({}: Props) {
   return (
-    <section>
+    <section className='mb-28 max-w-[50rem] text-center sm:mb-0 '>
         <div className='flex items-center justify-center'>  
             <div className='relative'>
+              <motion.div
+                initial={{opacity:0, scale:0}}
+                animate={{opacity:1, scale:1}}
+                transition={{duration: 0.2}}
+              >
                 <Image 
                 src="/portrait.jpg" 
                 alt="Portrait of Tom"
@@ -16,11 +24,34 @@ export default function Intro({}: Props) {
                 priority={true} 
                 className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
                 />
+              </motion.div>
 
-                <span className="absolute bottom-0 right-0 text-3xl">
-                  👋
-                </span>
+              <motion.span className="absolute bottom-0 right-0 text-3xl"
+              initial={{opacity:0, scale:0}}
+              animate={{opacity:1, scale:1}}
+              transition={{
+                type: 'spring',
+                stiffness: 125,
+                delay:0.1, 
+                duration: 0.7,
+              }}>
+                👋
+              </motion.span>
+              
             </div>
+        </div>
+        <motion.p 
+        className='mb-10 mt-10 px-4 text-xl font-medium !leading-[1.5] sm:text-3xl'
+        initial={{opacity: 0, y:100}}
+        animate={{opacity: 1, y:0}}>
+          <span className="font-bold"> Hello, I&apos;m Tom.  </span> I&apos;m a 
+          UBC Computer Science student of class 2024.
+          I have over <span className="font-bold">12 months</span> of work experience
+           in <span className='underline'>C# </span> and <span className='underline'>Full-stack</span>  development. 
+        </motion.p>
+
+        <div>
+          
         </div>
     </section>
   )
